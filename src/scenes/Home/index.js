54908acc0c6138a0d4b6055042992bebd6df0b28
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-24 16:16:15 +0430

    link all sections in HomePage

---
+++ src/scenes/Home/index.js
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import { Route, useRouteMatch } from "react-router-dom";
+
+// Sections
+import Blog from "./Blog";
+import About from "./About";
+import Intro from "./Intro";
+import Header from "./Header";
+import Service from "./Service";
+import Contact from "./Contact";
+import Portfolio from "./Portfolio";
+
+// Components
+import Helmet from "../../components/common/Helmet";
+import Switch from "../../components/common/Switch";
+import RedirectAs404 from "../../components/common/RedirectAs404";
+
+const routes = [
+    {
+        path: "/intro",
+        component: <Intro />,
+    },
+    {
+        path: "/about",
+        component: <About />,
+    },
+    {
+        path: "/service",
+        component: <Service />,
+    },
+    {
+        path: "/portfolio",
+        component: <Portfolio />,
+    },
+    {
+        path: "/blog",
+        component: <Blog />,
+    },
+    {
+        path: "/contact",
+        component: <Contact />,
+    },
+];
+
+function Home() {
+    let { path } = useRouteMatch();
+
+    useEffect(() => {
+        document.documentElement.className = "home-1 skin-1";
+        return () => {
+            document.documentElement.className = "";
+        };
+    });
+
+    return (
+        <div>
+            <Helmet title="Home 1" />
+            <Header />
+            <Switch>
+                {routes.map((item, index) => (
+                    <Route key={index} path={`${path}${item.path}`} exact>
+                        {item.component}
+                    </Route>
+                ))}
+                <Route component={RedirectAs404} />
+            </Switch>
+        </div>
+    );
+}
+
+export default Home;
