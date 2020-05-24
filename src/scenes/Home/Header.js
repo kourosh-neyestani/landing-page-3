07258dc5f7c152477cc8ab21560efd69ad761699
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-24 21:40:19 +0430

    update Header, add Menu, add Logo and social links

--- src/scenes/Home/Header.js
+++ src/scenes/Home/Header.js
@@ -46,7 +46,7 @@ function Header() {
         <>
             <header className="app-header app-header-1">
                 <div className="header-logo">
-                    <Link to={url}>NUXT</Link>
+                    <Link to={url}>Miro.</Link>
                 </div>
                 <button className="button button-menu" onClick={(e) => handleClick(e)}>
                     <span></span>
@@ -61,46 +61,27 @@ function Header() {
                         </li>
                     ))}
                 </ul>
-            </header>
-            <div className={`app-sidenav ${displaySidenav && "active"}`}>
-                <div className="sidenav-menu">
-                    <div className="logo">
-                        <Link to={url}>NUXT</Link>
-                    </div>
-                    <button className="button" onClick={(e) => handleClick(e)}></button>
-                    <ul className="sidenav-nav">
-                        {links.map((item, index) => (
-                            <li key={index} onClick={(e) => handleClick(e)}>
-                                <NavLink to={`${url}${item.path}`} activeClassName="active">
-                                    <span className="anchor">{item.title}</span>
-                                </NavLink>
-                            </li>
-                        ))}
+                <div class="header-follow">
+                    <ul>
+                        <li>
+                            <a href="/#">
+                                <i class="ti-twitter-alt"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/#">
+                                <i class="ti-github"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/#">
+                                <i class="ti-vimeo-alt"></i>
+                            </a>
+                        </li>
                     </ul>
-                    <div className="sidenav-links">
-                        <ul>
-                            <li>
-                                <a href="!#">
-                                    <FaTwitter className="icon" />
-                                </a>
-                            </li>
-                            <li>
-                                <a href="!#">
-                                    <FaGithub className="icon" />
-                                </a>
-                            </li>
-                            <li>
-                                <a href="!#">
-                                    <FaInstagram className="icon" />
-                                </a>
-                            </li>
-                        </ul>
-                        <p>Follow Me</p>
-                    </div>
+                    <p>Follow Me</p>
                 </div>
-                <span className="sidenav-close" />
-            </div>
-            <DemoOptions />
+            </header>
         </>
     );
 }
