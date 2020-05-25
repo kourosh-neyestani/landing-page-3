213b7a8aa939cd3cd6c1ebba121a3ae20cfa355e
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-25 16:13:46 +0430

    add Blog section, add posts, heading, images, etc

--- src/scenes/Home/Blog.js
+++ src/scenes/Home/Blog.js
@@ -1,8 +1,109 @@
 import React from "react";
+import Slider from "react-slick";
+import { Link } from "react-router-dom";
+import { Container } from "react-grid-system";
+
+// Components
+import Headline from "../../components/common/Headline";
+
+const posts = [
+    {
+        title: "What has happened to all of the Design ideas?",
+        image: "/assets/images/blog/1-1.jpg",
+        date: "May 15th, 2019",
+        link: "/single-post",
+        icon: "ti-light-bulb",
+    },
+    {
+        title: "The Best Sale Marketer Of The Next Year",
+        image: "/assets/images/blog/1-2.jpg",
+        date: "May 18th, 2019",
+        link: "/single-post",
+        icon: "ti-mobile",
+    },
+    {
+        title: "What gets in the way of greate Stategy",
+        image: "/assets/images/blog/1-3.jpg",
+        date: "May 21h, 2019",
+        link: "/single-post",
+        icon: "ti-paint-bucket",
+    },
+    {
+        title: "How to create great logo for your Business",
+        image: "/assets/images/blog/1-4.jpg",
+        date: "May 21th, 2019",
+        link: "/single-post",
+        icon: "ti-light-bulb",
+    },
+];
 
 function Blog() {
+    let sliderSettings = {
+        dots: true,
+        speed: 500,
+        arrows: false,
+        infinite: false,
+        initialSlide: 0,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerPadding: 30,
+        draggable: false,
+        responsive: [
+            {
+                breakpoint: 1500,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                },
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+
     return (
-        <h1>Blog</h1>
+        <section className="section section-blog section-blog-1">
+            <div className="display-spacing">
+                <Container className="container">
+                    <Headline label="Blog" title="Let's See My Work" divider_1={true} position="center" />
+                    <Slider className="el-slider el-slider-plr--15" {...sliderSettings}>
+                        {posts.map((item, index) => (
+                            <div key={index} className="post-item">
+                                <Link to={`/home-2${item.link}`}>
+                                    <div class="post-image" style={{backgroundImage: `url(${item.image})`}}></div>
+                                </Link>
+                                <div className="post-content">
+                                    <div className="el-icon">
+                                        <span className={`el-icon-icon ${item.icon}`}></span>
+                                    </div>
+                                    <div className="post-data">
+                                        <h4 className="post-title">
+                                            <Link to={`/home-1${item.link}`}>{item.title}</Link>
+                                        </h4>
+                                        <div className="post-date">
+                                            <i className="ti-time"></i>
+                                            {item.date}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </Slider>
+                </Container>
+            </div>
+        </section>
     );
 }
 
