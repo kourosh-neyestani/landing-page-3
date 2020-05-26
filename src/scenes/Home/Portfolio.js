7f17c3e48e8d7adbec3860aa740ce203eb338d2c
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-26 12:14:38 +0430

    add Portfolio lightbox

--- src/scenes/Home/Portfolio.js
+++ src/scenes/Home/Portfolio.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useRouteMatch } from "react-router-dom";
 import { Container, Row, Col } from "react-grid-system";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 // Components
 import Headline from "../../components/common/Headline";
@@ -14,70 +15,70 @@ const portfolio = [
                 title: "Crearive Design",
                 link: "/portfolio-details",
                 subtitle: "Web Design",
-                image: "/assets/images/portfolio/1-1.jpg",
+                src: "/assets/images/portfolio/1-1.jpg",
                 thumbnail: "/assets/images/portfolio/1-1.jpg",
             },
             {
                 title: "Crearive Design",
                 link: "/portfolio-details",
                 subtitle: "Web Design",
-                image: "/assets/images/portfolio/1-2.jpg",
+                src: "/assets/images/portfolio/1-2.jpg",
                 thumbnail: "/assets/images/portfolio/1-2.jpg",
             },
             {
                 title: "Crearive Design",
                 link: "/portfolio-details",
                 subtitle: "Mobile Apps",
-                image: "/assets/images/portfolio/1-3.jpg",
+                src: "/assets/images/portfolio/1-3.jpg",
                 thumbnail: "/assets/images/portfolio/1-3.jpg",
             },
             {
                 title: "Crearive Design",
                 subtitle: "Mobile Apps",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-4.jpg",
+                src: "/assets/images/portfolio/1-4.jpg",
                 thumbnail: "/assets/images/portfolio/1-4.jpg",
             },
             {
                 title: "Crearive Design",
                 subtitle: "Mobile Apps",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-5.jpg",
+                src: "/assets/images/portfolio/1-5.jpg",
                 thumbnail: "/assets/images/portfolio/1-5.jpg",
             },
             {
                 title: "Crearive Design",
                 subtitle: "Branding",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-6.jpg",
+                src: "/assets/images/portfolio/1-6.jpg",
                 thumbnail: "/assets/images/portfolio/1-6.jpg",
             },
             {
                 title: "Crearive Design",
                 subtitle: "Branding",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-7.jpg",
+                src: "/assets/images/portfolio/1-7.jpg",
                 thumbnail: "/assets/images/portfolio/1-7.jpg",
             },
             {
                 title: "Crearive Design",
                 subtitle: "Branding",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-8.jpg",
+                src: "/assets/images/portfolio/1-8.jpg",
                 thumbnail: "/assets/images/portfolio/1-8.jpg",
             },
             {
                 title: "Crearive Design",
                 subtitle: "Branding",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-9.jpg",
+                src: "/assets/images/portfolio/1-9.jpg",
                 thumbnail: "/assets/images/portfolio/1-9.jpg",
             },
             {
                 title: "Crearive Design",
                 subtitle: "Branding",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-10.jpg",
+                src: "/assets/images/portfolio/1-10.jpg",
                 thumbnail: "/assets/images/portfolio/1-10.jpg",
             },
         ],
@@ -89,28 +90,28 @@ const portfolio = [
                 title: "Crearive Design",
                 link: "/portfolio-details",
                 subtitle: "Web Design",
-                image: "/assets/images/portfolio/1-1.jpg",
+                src: "/assets/images/portfolio/1-1.jpg",
                 thumbnail: "/assets/images/portfolio/1-1.jpg",
             },
             {
                 title: "Crearive Design",
                 link: "/portfolio-details",
                 subtitle: "Web Design",
-                image: "/assets/images/portfolio/1-2.jpg",
+                src: "/assets/images/portfolio/1-2.jpg",
                 thumbnail: "/assets/images/portfolio/1-2.jpg",
             },
             {
                 title: "Crearive Design",
                 link: "/portfolio-details",
                 subtitle: "Web Design",
-                image: "/assets/images/portfolio/1-3.jpg",
+                src: "/assets/images/portfolio/1-3.jpg",
                 thumbnail: "/assets/images/portfolio/1-3.jpg",
             },
             {
                 title: "Crearive Design",
                 link: "/portfolio-details",
                 subtitle: "Web Design",
-                image: "/assets/images/portfolio/1-4.jpg",
+                src: "/assets/images/portfolio/1-4.jpg",
                 thumbnail: "/assets/images/portfolio/1-4.jpg",
             },
         ],
@@ -122,21 +123,21 @@ const portfolio = [
                 title: "Crearive Design",
                 subtitle: "Mobile Apps",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-5.jpg",
+                src: "/assets/images/portfolio/1-5.jpg",
                 thumbnail: "/assets/images/portfolio/1-5.jpg",
             },
             {
                 title: "Crearive Design",
                 subtitle: "Mobile Apps",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-6.jpg",
+                src: "/assets/images/portfolio/1-6.jpg",
                 thumbnail: "/assets/images/portfolio/1-6.jpg",
             },
             {
                 title: "Crearive Design",
                 subtitle: "Mobile Apps",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-7.jpg",
+                src: "/assets/images/portfolio/1-7.jpg",
                 thumbnail: "/assets/images/portfolio/1-7.jpg",
             },
         ],
@@ -148,36 +149,75 @@ const portfolio = [
                 title: "Crearive Design",
                 subtitle: "Branding",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-8.jpg",
+                src: "/assets/images/portfolio/1-8.jpg",
                 thumbnail: "/assets/images/portfolio/1-8.jpg",
             },
             {
                 title: "Crearive Design",
                 subtitle: "Branding",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-9.jpg",
+                src: "/assets/images/portfolio/1-9.jpg",
                 thumbnail: "/assets/images/portfolio/1-9.jpg",
             },
             {
                 title: "Crearive Design",
                 subtitle: "Branding",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-9.jpg",
+                src: "/assets/images/portfolio/1-9.jpg",
                 thumbnail: "/assets/images/portfolio/1-9.jpg",
             },
             {
                 title: "Crearive Design",
                 subtitle: "Branding",
                 link: "/portfolio-details",
-                image: "/assets/images/portfolio/1-10.jpg",
+                src: "/assets/images/portfolio/1-10.jpg",
                 thumbnail: "/assets/images/portfolio/1-10.jpg",
             },
         ],
     },
 ];
 
+const images = [
+    {
+        src: "/assets/images/portfolio/1-1.jpg",
+    },
+    {
+        src: "/assets/images/portfolio/1-2.jpg",
+    },
+    {
+        src: "/assets/images/portfolio/1-3.jpg",
+    },
+    {
+        src: "/assets/images/portfolio/1-4.jpg",
+    },
+    {
+        src: "/assets/images/portfolio/1-5.jpg",
+    },
+    {
+        src: "/assets/images/portfolio/1-6.jpg",
+    },
+    {
+        src: "/assets/images/portfolio/1-7.jpg",
+    },
+    {
+        src: "/assets/images/portfolio/1-8.jpg",
+    },
+    {
+        src: "/assets/images/portfolio/1-9.jpg",
+    },
+    {
+        src: "/assets/images/portfolio/1-10.jpg",
+    },
+];
+
 function Portfolio() {
-    const { path } = useRouteMatch();
+    const [lightboxIsOpen, setLightboxIsOpen] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    const toggleLightbox = (index) => {
+        setSelectedIndex(index);
+        setLightboxIsOpen(!lightboxIsOpen);
+    };
 
     return (
         <section className="section section-portfolio section-portfolio-1">
@@ -194,20 +234,18 @@ function Portfolio() {
                             <TabPanel key={categoryIndex}>
                                 <Row className="row-center mb--30">
                                     {category.items.map((item, index) => (
-                                        <Col key={index} xs={6} sm={6} md={6} lg={3} xl={3}>
+                                        <Col key={index} xs={6} sm={6} md={6} lg={3} xl={3} onClick={() => toggleLightbox(index)}>
                                             <div className="portfolio-item">
-                                                <Link to={`${path}${item.link}`.replace(/([^:])(\/\/+)/g, "$1/")}>
-                                                    <div className="portfolio-card">
-                                                        <div className="image overlay-image" style={{ backgroundImage: `url(${item.image})` }} />
-                                                        <div className="content">
-                                                            <button className="button button-circle button-icon button-circle-md button-primary">
-                                                                <span className="el-icon-icon ti-fullscreen"></span>
-                                                            </button>
-                                                            <span>{item.title}</span>
-                                                            <h3>{item.subtitle}</h3>
-                                                        </div>
+                                                <div className="portfolio-card">
+                                                    <div className="image overlay-image" style={{ backgroundImage: `url(${item.src})` }} />
+                                                    <div className="content">
+                                                        <button className="button button-circle button-icon button-circle-md button-primary">
+                                                            <span className="el-icon-icon ti-fullscreen"></span>
+                                                        </button>
+                                                        <span>{item.title}</span>
+                                                        <h3>{item.subtitle}</h3>
                                                     </div>
-                                                </Link>
+                                                </div>
                                             </div>
                                         </Col>
                                     ))}
@@ -216,6 +254,13 @@ function Portfolio() {
                         ))}
                     </Tabs>
                 </Container>
+                <ModalGateway>
+                    {lightboxIsOpen ? (
+                        <Modal onClose={toggleLightbox}>
+                            <Carousel currentIndex={selectedIndex} views={images} />
+                        </Modal>
+                    ) : null}
+                </ModalGateway>
             </div>
         </section>
     );
