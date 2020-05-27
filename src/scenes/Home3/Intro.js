1b9bff3dc1bf643a7afb548af6d7889c7982769e
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-27 23:02:48 +0430

    add Hero-3

--- src/scenes/Home3/Intro.js
+++ src/scenes/Home3/Intro.js
@@ -1,74 +1,30 @@
 import React from "react";
-import Particles from "react-particles-js";
+import TextLoop from "react-text-loop";
 import { Container } from "react-grid-system";
 
 function Intro() {
     return (
-        <section className="section section-hero section-hero-2 overlay-image" style={{ backgroundImage: `url(/assets/images/hero/2-1.jpg)` }}>
-            <span class="el-circle-side right"></span>
-            <Particles
-                params={{
-                    particles: {
-                        number: {
-                            value: 300,
-                            density: {
-                                enable: true,
-                            },
-                        },
-                        size: {
-                            value: 5,
-                            random: true,
-                        },
-                        move: {
-                            direction: "bottom",
-                            out_mode: "out",
-                        },
-                        line_linked: {
-                            enable: false,
-                        },
-                    },
-                    interactivity: {
-                        events: {
-                            onhover: {
-                                enable: true,
-                                mode: "bubble",
-                            },
-                            onclick: {
-                                enable: true,
-                                mode: "repulse",
-                            },
-                        },
-                        modes: {
-                            bubble: {
-                                distance: 250,
-                                duration: 2,
-                                size: 3,
-                            },
-                            repulse: {
-                                distance: 400,
-                                duration: 4,
-                            },
-                        },
-                    },
-                }}
-            />
+        <section className="section section-hero section-hero-3 overlay-image" style={{ backgroundImage: `url(/assets/images/hero/3-1.jpg)` }}>
             <div className="display-center">
                 <Container>
                     <div className="el-heading">
                         <p className="el-icon">
-                            <span className="el-icon-title">Welcome on our Beauty Clinic</span>
+                            <span className="el-icon-title">Hello.</span>
                         </p>
                         <h1>
-                            We make hair
-                            <br />
-                            beautiful & unique
+                            John Woo is a{" "}
+                            <TextLoop>
+                                <span>Documentary Filmmaker</span>
+                                <span>Traveller Blogger</span>
+                                <span>Documentary Photographer</span>
+                            </TextLoop>
                         </h1>
                         <button className="button button-lg button-primary">
                             <span className="wave"></span>
-                            <span className="text">Book a visit</span>
+                            <span className="text">My Portfolio</span>
                         </button>
-                        <button className="button button-lg button-dark">
-                            <span className="text">Read More</span>
+                        <button className="button button-lg button-light">
+                            <span className="text text-primary">Read More</span>
                         </button>
                     </div>
                 </Container>
