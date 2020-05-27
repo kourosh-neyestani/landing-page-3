8cc6d11fb0385c61704ed5f53becb0a15cf454aa
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-05-28 00:10:47 +0430

    update Hero-6

--- src/scenes/Home6/Intro.js
+++ src/scenes/Home6/Intro.js
@@ -1,78 +1,139 @@
 import React from "react";
-import TextLoop from "react-text-loop";
 import Particles from "react-particles-js";
 import { Container } from "react-grid-system";
 
 function Intro() {
     return (
-        <section className="section section-hero section-hero-1 overlay-image" style={{ backgroundImage: `url(/assets/images/hero/1-1.jpg)` }}>
+        <section className="section section-hero section-hero-6 overlay-image" style={{ backgroundImage: `url(/assets/images/hero/6-1.jpg)` }}>
             <Particles
                 params={{
                     particles: {
                         number: {
-                            value: 300,
+                            value: 170,
                             density: {
                                 enable: true,
+                                value_area: 300,
+                            },
+                        },
+                        color: {
+                            value: "#fff",
+                        },
+                        shape: {
+                            type: "circle",
+                            stroke: {
+                                width: 0,
+                                color: "#000000",
+                            },
+                            polygon: {
+                                nb_sides: 5,
+                            },
+                            image: {
+                                src: "img/github.svg",
+                                width: 100,
+                                height: 100,
+                            },
+                        },
+                        opacity: {
+                            value: 0.6,
+                            random: true,
+                            anim: {
+                                enable: false,
+                                speed: 1,
+                                opacity_min: 0.5,
+                                sync: false,
                             },
                         },
                         size: {
-                            value: 5,
+                            value: 1,
                             random: true,
-                        },
-                        move: {
-                            direction: "bottom",
-                            out_mode: "out",
+                            anim: {
+                                enable: false,
+                                speed: 40,
+                                size_min: 0.1,
+                                sync: false,
+                            },
                         },
                         line_linked: {
                             enable: false,
+                            distance: 500,
+                            color: "#ffffff",
+                            opacity: 0.4,
+                            width: 2,
+                        },
+                        move: {
+                            enable: true,
+                            speed: 6,
+                            direction: "left",
+                            random: false,
+                            straight: false,
+                            out_mode: "out",
+                            bounce: false,
+                            attract: {
+                                enable: false,
+                                rotateX: 600,
+                                rotateY: 1200,
+                            },
                         },
                     },
                     interactivity: {
+                        detect_on: "canvas",
                         events: {
                             onhover: {
                                 enable: true,
-                                mode: "bubble"
+                                mode: "bubble",
                             },
                             onclick: {
                                 enable: true,
-                                mode: "repulse"
-                            }
+                                mode: "repulse",
+                            },
+                            resize: true,
                         },
                         modes: {
+                            grab: {
+                                distance: 400,
+                                line_linked: {
+                                    opacity: 0.5,
+                                },
+                            },
                             bubble: {
-                                distance: 250,
-                                duration: 2,
-                                size: 3,
+                                distance: 400,
+                                size: 4,
+                                duration: 0.3,
+                                opacity: 1,
+                                speed: 3,
                             },
                             repulse: {
-                                distance: 400,
-                                duration: 4
-                            }
-                        }
-                    }
+                                distance: 200,
+                                duration: 0.4,
+                            },
+                            push: {
+                                particles_nb: 4,
+                            },
+                            remove: {
+                                particles_nb: 2,
+                            },
+                        },
+                    },
+                    retina_detect: true,
                 }}
+                className="el-particles"
             />
             <div className="display-center">
                 <Container>
                     <div className="el-heading">
                         <p className="el-icon">
-                            <span className="el-icon-title">I'm Miranda Smith.</span>
+                            <span className="el-icon-title">Miro Clinic</span>
                         </p>
                         <h1>
-                            I'm a{" "}
-                            <TextLoop>
-                                <span>UI/UX Designer</span>
-                                <span>Graphic Designer</span>
-                                <span>Web Developer</span>
-                            </TextLoop>
+                            Welcome to the
                             <br />
-                            Based in Los Angeles
+                            Miro Medical Clinic
                         </h1>
                         <button className="button button-lg button-primary">
-                            <span className="wave"></span>
-                            <span className="text">My Portfolio</span>
+                            <span className="text text-dark">Book a visit</span>
                         </button>
-                        <button className="button button-lg">
+                        <button className="button button-lg button-dark">
+                            <span className="wave"></span>
                             <span className="text">Read More</span>
                         </button>
                     </div>
